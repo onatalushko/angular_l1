60b4f3dca22ca084c1abe081898554411d67f14b
Oleg Natalushko <olegnatalushko@gmail.com>
2021-08-16 16:21:44 +0300

    car component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarComponent } from "./composenents/car/car.component";
+import {ContactComponent} from "./components/contact/contact.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CarComponent },
+  { path: 'contacts', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
